a63b220ff7a620c0a9d3bad1f0aa0ff0f06344c6
SUMAN RANA <sumanrana6297@gmail.com>
2024-09-29 23:35:28 +0700

    Add files via upload

---
+++ day3.java
@@ -0,0 +1,10 @@
+public class sumanrana{
+    public static void main (String[] args){
+        System.out.print("the sum of three numbers is :-  ");
+        int num1 = 3;
+        int num2 = 6;
+        int num3 = 6;
+        int total = num1 + num2 + num3;
+        System.out.print(total);
+    }
+    }
---
+++ day4.java
@@ -0,0 +1,19 @@
+public class day4 {
+    public static void main (String[] args){
+         
+        byte age = 56;
+        char ch = 'A';
+        float f1 = 5.6f;
+        short age1 = 56;
+        long age3 = 5666666L;
+        boolean a = true;
+      System.out.println(age+f1+age1+age3);
+       String fdr = "suman rana";
+       System.out.println(fdr);
+
+
+
+
+
+    }
+}
