47ef83e815c23d3ee81bdbae20a75cb1e773833f
SUMAN RANA <sumanrana6297@gmail.com>
2024-09-30 22:11:58 +0700

    Add files via upload

---
+++ day5.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class day5{
+  public static void main (String[]args){
+
+ System.out.println("taking input from the user");
+ Scanner sc = new Scanner(System.in);
+ System.out.println("enter the 1st number");
+ float a = sc.nextFloat();
+ System.out.println("enter the second number");
+ float b = sc.nextFloat();
+float sum = a+b;
+ System.out.println("sum  of this two number is");
+ System.out.println(sum);
+
+
+  }}
