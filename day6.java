ac4241fd227dd262d17ba559ad27895e3b6fb54c
SUMAN RANA <sumanrana6297@gmail.com>
2024-10-02 01:23:26 +0700

    Add files via upload

---
+++ day6.java
@@ -0,0 +1,40 @@
+
+import java.util.Scanner;
+public class day6 {
+
+    public static void main(String[] args) {
+        
+    
+
+      System.out.println("taking input from the user");
+      
+      Scanner sc = new Scanner(System.in);
+            System.out.println("enter the 1st number fro addition?:");
+       int age = sc.nextInt();
+       System.out.println("enter the second number  for addition");
+       int age1 = sc.nextInt();
+      
+             int sum =age+age1;
+             System.out.println(sum);
+              
+Scanner sc1 = new Scanner(System.in);
+System.out.println("enter the 1st number for subtraction ?");
+ int age2 = sc.nextInt();
+ System.out.println("enter the second number for subtraction ?");
+ int age3 = sc.nextInt();
+ 
+ int sum1 = age2-age3;
+ System.out.println(sum1);
+       
+      
+       
+
+      
+
+
+     
+       
+
+    }   
+
+}
