cc0f82a82f7969daab3215629ea06b4175992942
SUMAN RANA <sumanrana6297@gmail.com>
2024-10-07 00:06:01 +0530

    Add files via upload

---
+++ day7.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class day7 {
+
+ public static void main(String[] args) {
+
+     
+
+// System.out.println("lets convert from km to m...");
+// System.out.println("enter your km value");
+// Scanner sc = new Scanner(System.in);
+// double kilometers = sc.nextDouble();
+// double meters = kilometers*1000;
+// System.out.println("the value of your kilometer is " + meters + "meter" );
+
+//    float subject = 23;
+//    float subject1 = 56;
+//    float subject3 = 65;
+//    float cgpa = (subject+subject1+subject3)/100;
+//    System.out.println(cgpa);
+
+
+
+
+
+
+ }}
+
+
+
+// System.out.println("what is your name");
+// Scanner sc = new Scanner(System.in);
+// String name = sc.next();
+//  System.out.println("have a great day " + name +  "a nice to meet you");
