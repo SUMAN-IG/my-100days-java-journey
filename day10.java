83a902d1ca740c1d6c2f8e06d679ceef53654a5e
SUMAN RANA <sumanrana6297@gmail.com>
2024-10-08 00:27:46 +0530

    Add files via upload

---
+++ day10.java
@@ -0,0 +1,32 @@
+public class day10 {
+    public static void main(String[] args) {
+      
+//             int a = 4;      //arithmatic operator
+
+
+//             int b = 6 %a;       // modulo operator
+
+
+//             int b = 4;
+//             b += 4;
+//        System.out.println(b);
+//        System.out.println(56<3);                      //comparison operator
+//        System.out.println(64>4 && 64>6);                   // and operator
+// System.out.println(45>3 || 45<5);
+
+// int a = 9/3-9*4;             // associativity and precedence
+// System.out.println(a);
+
+
+
+
+     // QUIZ TIME
+    int a = 1;
+    int b = 4;
+int c = 5;
+     int k  = b*b -(4*a*b*c)/(2*a);
+System.out.println(k);
+
+    }
+
+}
