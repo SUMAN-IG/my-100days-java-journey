4bcd89af06c6c01d04d3af48dbffced20e1a61fa
SUMAN RANA <sumanrana6297@gmail.com>
2024-10-20 00:49:09 +0530

    Add files via upload

---
+++ day19.java
@@ -0,0 +1,47 @@
+public class day19 {
+    public static void main(String[] args) {
+        
+// // its a while loop condition
+
+// int i1= 100;
+//     while (i1 <=200) {
+//     System.out.println(i1);
+//     i1++;
+// }
+ // its an do while loop conditions
+
+//   int a = 1;
+//      do {
+//         System.out.println(a);
+//         a++;
+//      } while (a<=10);
+
+
+//      // its an for loop condition
+
+//    for (int b=100; b>0;b-- ){
+//     System.out.println(b);
+   
+// //     } 
+    
+//  for( int i=0; i<=10; i++){
+//     System.out.println(i);
+// System.out.println("JAY BABA KI" );
+//                                                // use of break statement 
+// if (i==2) {
+//     System.out.println("bye bye loop");
+//     break;
+// }}
+
+int d=0;
+   do {
+    System.out.println(d);
+   }while (d<=5);{
+        System.out.println("end of for loop");
+    }
+   if (d==2) {
+    System.out.println("end o floop");
+    d++;
+    
+   }
+   }}
