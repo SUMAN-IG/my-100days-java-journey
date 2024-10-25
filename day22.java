d2178b233ec8bc6931906ffa95ed642c610fcc87
SUMAN RANA <sumanrana6297@gmail.com>
2024-10-26 01:01:34 +0530

    Add files via upload

---
+++ day22.java
@@ -0,0 +1,27 @@
+public class day22 {
+    public static void main(String[] args) {
+// there are 3 types to make arrays in java
+
+  // 1. declearation and  memeoery allocastion 
+//int [] marks=new int[5]
+
+         
+//2. declearation and then memeoery allocastion 
+
+//     int [] marks = new int[5];
+// //initialize it
+
+//         marks[0]=45;
+//         marks[1]=67;
+//         marks[2]=78;
+//         marks[3]=34;
+//         marks[4]=33;
+//         System.out.println(marks[3]);
+        
+//  3. declaration memory allocation and initilization together
+int [] marks1={23,23,35,5,4};
+    System.out.println(marks1[3]);
+
+        //
+    }
+}
